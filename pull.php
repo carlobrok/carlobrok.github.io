2b2bc9c7526dceca5c25a7ae5e61daca672c6d24
carlobrok <carlo.brokering@outlook.de>
2020-07-08 18:37:11 +0200

    debug

--- pull.php
+++ pull.php
@@ -1,9 +1,14 @@
+
 <?php
 
 // Use in the “Post-Receive URLs” section of your GitHub repo.
+
+echo getcwd()
+
 
 if ( $_POST['payload'] ) {
 shell_exec( ‘git reset --hard HEAD && git pull’ );
 }
 
-?>hi
+
+?>
