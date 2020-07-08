d7b89b310f4da04ed385672bfd7d4d5ad3d5ec14
carlobrok <carlo.brokering@outlook.de>
2020-07-09 01:29:12 +0200

    remove and rename

--- deploy/deploy.php
+++ deploy/deploy.php
@@ -1,12 +1,87 @@
 <?php
+$content = file_get_contents("php://input");
+$json    = json_decode($content, true);
+$file    = fopen(LOGFILE, "a");
+$time    = time();
+$token   = false;
+$sha     = false;
+$DIR     = preg_match("/\/$/", DIR) ? DIR : DIR . "/";
 
-define("REMOTE_REPOSITORY", "git@github.com:carlobrok/website.git");   // The SSH URL to your repository
-define("DIR", "/home/ubuntu/website");                          // The path to your repostiroy; this must begin with a forward slash (/)
-define("BRANCH", "refs/heads/master");                                 // The branch route
-define("LOGFILE", "deploy.log");                                       // The name of the file you want to log to.
-define("GIT", "/usr/bin/git");                                         // The path to the git executable
-define("MAX_EXECUTION_TIME", 180);                                     // Override for PHP's max_execution_time (may need set in php.ini)
-define("BEFORE_PULL", "/usr/bin/git reset --hard");                                             // A command to execute before pulling
-define("AFTER_PULL", "");                                              // A command to execute after successfully pulling
+$pTOKEN  = file_get_contents("token.txt", true);
+$pTOKEN  = substr($pTOKEN, 0, -1);
 
-require_once("deployer.php");
+
+
+// retrieve the token
+if (!$token && isset($_SERVER["HTTP_X_HUB_SIGNATURE"])) {
+    list($algo, $token) = explode("=", $_SERVER["HTTP_X_HUB_SIGNATURE"], 2) + array("", "");
+} elseif (isset($_SERVER["HTTP_X_GITLAB_TOKEN"])) {
+    $token = $_SERVER["HTTP_X_GITLAB_TOKEN"];
+} elseif (isset($_GET["token"])) {
+    $token = $_GET["token"];
+}
+
+
+// retrieve the checkout_sha
+if (isset($json["checkout_sha"])) {
+    $sha = $json["checkout_sha"];
+} elseif (isset($_SERVER["checkout_sha"])) {
+    $sha = $_SERVER["checkout_sha"];
+} elseif (isset($_GET["sha"])) {
+    $sha = $_GET["sha"];
+}
+
+// write the time to the log
+date_default_timezone_set("UTC");
+fputs($file, date("d-m-Y (H:i:s)", $time) . "\n");
+
+// specify that the response does not contain HTML
+header("Content-Type: text/plain");
+
+// use user-defined max_execution_time
+if (!empty(MAX_EXECUTION_TIME)) {
+    ini_set("max_execution_time", MAX_EXECUTION_TIME);
+}
+
+// function to forbid access
+function forbid($file, $reason) {
+    // format the error
+    $error = "=== ERROR: " . $reason . " ===\n*** ACCESS DENIED ***\n";
+
+    // forbid
+    http_response_code(403);
+
+    // write the error to the log and the body
+    fputs($file, $error . "\n\n");
+    echo $error;
+
+    // close the log
+    fclose($file);
+
+    // stop executing
+    exit;
+}
+
+// Check for a GitHub signature
+if (!empty($pTOKEN) && isset($_SERVER["HTTP_X_HUB_SIGNATURE"]) && $token !== hash_hmac($algo, $content, $pTOKEN)) {
+    forbid($file, "X-Hub-Signature does not match TOKEN");
+// Check for a GitLab token
+} elseif (!empty($pTOKEN) && isset($_SERVER["HTTP_X_GITLAB_TOKEN"]) && $token !== $pTOKEN) {
+    forbid($file, "X-GitLab-Token does not match TOKEN");
+// Check for a $_GET token
+} elseif (!empty($pTOKEN) && isset($_GET["token"]) && strcmp($token,$pTOKEN) !== 0) {
+    forbid($file, "\$_GET[\"token\"] does not match TOKEN");
+// if none of the above match, but a token exists, exit
+} elseif (!empty($pTOKEN) && !isset($_SERVER["HTTP_X_HUB_SIGNATURE"]) && !isset($_SERVER["HTTP_X_GITLAB_TOKEN"]) && !isset($_GET["token"])) {
+    forbid($file, "No token detected");
+} else {
+  ob_start();
+  passthru("./update.sh");
+  $output = ob_end_contents();
+  echo $output,
+  echo "Successfully updated repo";
+}
+
+// close the log
+fputs($file, "\n\n" . PHP_EOL);
+fclose($file);
